ce9125b94a472b3a851c4282d1e5d233c15cf294
Alexandru Tiniuc <tiniuc.alexandru@gmail.com>
2019-10-24 21:58:43 +0100

    use pitch_calc crate instead

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-#[macro_use]
 extern crate enum_primitive_derive;
+extern crate num_traits;
 
 mod music_theory;
-use music_theory::Note;
+use pitch_calc::Letter::*;
 
 fn main() {
-    println!("{:?}", Note::C);
-    println!("{:?}", Note::C + 7);
-    println!("{:?}", Note::C + 12);
-    println!("{:?}", Note::C + 48);
-    println!("{:?}", Note::C - 2);
-    println!("{:?}", Note::C - 7);
-    println!("{:?}", Note::C - 22);
+    println!("{:?}", C);
+    println!("{:?}", C + 7);
+    println!("{:?}", C + 12);
+    println!("{:?}", C + 48);
+    println!("{:?}", C - 2);
+    println!("{:?}", C - 7);
+    println!("{:?}", C - 22);
 }
--- src/music_theory.rs
+++ src/music_theory.rs
@@ -1,73 +1,21 @@
-extern crate num_traits;
-
-use num_traits::{FromPrimitive, ToPrimitive};
-use std::ops;
-
-#[derive(Debug, PartialEq, Eq, Primitive)]
-pub enum Note {
-    A = 0,
-    Bb = 1,
-    B = 2,
-    C = 3,
-    Db = 4,
-    D = 5,
-    Eb = 6,
-    E = 7,
-    F = 8,
-    Gb = 9,
-    G = 10,
-    Ab = 11,
-}
-impl ops::Add<i64> for Note {
-    type Output = Note;
-
-    fn add(self, other: i64) -> Note {
-        let note = Note::to_i64(&self).expect("Note::to_i64 failed!");
-        let transposed = (note + other).rem_euclid(12);
-
-        Note::from_i64(transposed).expect("Note::from_i64 failed!")
-    }
-}
-
-impl ops::Sub<i64> for Note {
-    type Output = Note;
-
-    fn sub(self, other: i64) -> Note {
-        self + (-other)
-    }
-}
+#[allow(unused_imports)]
+use pitch_calc::Letter::*;
 
 #[allow(dead_code)]
 pub enum Interval {
-    Unison,
-    Min2nd,
-    Maj2nd,
-    Min3rd,
-    Maj3rd,
-    Per4th,
-    Dim5th,
-    Per5th,
-    Min6th,
-    Maj6th,
-    Min7th,
-    Maj7th,
-    Octave,
-}
-
-impl ops::Add<Interval> for Note {
-    type Output = Note;
-
-    fn add(self, other: Interval) -> Note {
-        self + other as i64
-    }
-}
-
-impl ops::Sub<Interval> for Note {
-    type Output = Note;
-
-    fn sub(self, other: Interval) -> Note {
-        self - other as i64
-    }
+    Unison = 0,
+    Min2nd = 1,
+    Maj2nd = 2,
+    Min3rd = 3,
+    Maj3rd = 4,
+    Per4th = 5,
+    Dim5th = 6,
+    Per5th = 7,
+    Min6th = 8,
+    Maj6th = 9,
+    Min7th = 10,
+    Maj7th = 11,
+    Octave = 12,
 }
 
 #[cfg(test)]
@@ -77,32 +25,32 @@ mod tests {
 
     #[test]
     fn small_transposition() {
-        assert_eq!(Note::G, Note::C + 7);
+        assert_eq!(G, C + 7);
     }
 
     #[test]
     fn big_transposition() {
-        assert_eq!(Note::C, Note::C + 12);
-        assert_eq!(Note::C, Note::C + 24);
-        assert_eq!(Note::C, Note::C + 144);
+        assert_eq!(C, C + 12);
+        assert_eq!(C, C + 24);
+        assert_eq!(C, C + 144);
     }
 
     #[test]
     fn transpose_down() {
-        assert_eq!(Note::Db, Note::D - 1);
-        assert_eq!(Note::C, Note::G - 7);
-        assert_eq!(Note::C, Note::C - 24);
+        assert_eq!(Db, D - 1);
+        assert_eq!(C, G - 7);
+        assert_eq!(C, C - 24);
     }
 
     #[test]
     fn interval_maths() {
-        assert_eq!(Note::C, Note::F + Per5th);
-        assert_eq!(Note::C, Note::C + Octave);
-        assert_eq!(Note::C, Note::C + Octave + Octave);
+        assert_eq!(C, F + Per5th as i64);
+        assert_eq!(C, C + Octave as i64);
+        assert_eq!(C, C + Octave as i64 + Octave as i64);
 
-        assert_eq!(Note::C, Note::G - Per5th);
-        assert_eq!(Note::C, Note::C - Octave);
-        assert_eq!(Note::C, Note::C - Per4th - Per5th);
-        assert_eq!(Note::C - Min2nd, Note::C + Maj7th);
+        assert_eq!(C, G - Per5th as i64);
+        assert_eq!(C, C - Octave as i64);
+        assert_eq!(C, C - Per4th as i64 - Per5th as i64);
+        assert_eq!(C - Min2nd as i64, C + Maj7th as i64);
     }
 }
