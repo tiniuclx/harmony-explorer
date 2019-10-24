f7d46139a7c96f36da4f147790bbaeb01f70e3ef
Alexandru Tiniuc <tiniuc.alexandru@gmail.com>
2019-10-24 22:15:04 +0100

    refactor Interval type to be plain old int

--- src/music_theory.rs
+++ src/music_theory.rs
@@ -1,56 +1,36 @@
-#[allow(unused_imports)]
-use pitch_calc::Letter::*;
-
-#[allow(dead_code)]
-pub enum Interval {
-    Unison = 0,
-    Min2nd = 1,
-    Maj2nd = 2,
-    Min3rd = 3,
-    Maj3rd = 4,
-    Per4th = 5,
-    Dim5th = 6,
-    Per5th = 7,
-    Min6th = 8,
-    Maj6th = 9,
-    Min7th = 10,
-    Maj7th = 11,
-    Octave = 12,
+#[allow(dead_code, non_upper_case_globals)]
+pub mod interval {
+    type Interval = i64;
+    pub const Unison: Interval = 0;
+    pub const Min2nd: Interval = 1;
+    pub const Maj2nd: Interval = 2;
+    pub const Min3rd: Interval = 3;
+    pub const Maj3rd: Interval = 4;
+    pub const Per4th: Interval = 5;
+    pub const Dim5th: Interval = 6;
+    pub const Per5th: Interval = 7;
+    pub const Min6th: Interval = 8;
+    pub const Maj6th: Interval = 9;
+    pub const Min7th: Interval = 10;
+    pub const Maj7th: Interval = 11;
+    pub const Octave: Interval = 12;
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
-    use Interval::*;
-
-    #[test]
-    fn small_transposition() {
-        assert_eq!(G, C + 7);
-    }
-
-    #[test]
-    fn big_transposition() {
-        assert_eq!(C, C + 12);
-        assert_eq!(C, C + 24);
-        assert_eq!(C, C + 144);
-    }
-
-    #[test]
-    fn transpose_down() {
-        assert_eq!(Db, D - 1);
-        assert_eq!(C, G - 7);
-        assert_eq!(C, C - 24);
-    }
+    use interval::*;
+    #[allow(unused_imports)]
+    use pitch_calc::Letter::*;
 
     #[test]
     fn interval_maths() {
-        assert_eq!(C, F + Per5th as i64);
-        assert_eq!(C, C + Octave as i64);
-        assert_eq!(C, C + Octave as i64 + Octave as i64);
+        assert_eq!(C, F + Per5th);
+        assert_eq!(C, C + Octave);
+        assert_eq!(C, C + Octave + Octave);
 
-        assert_eq!(C, G - Per5th as i64);
-        assert_eq!(C, C - Octave as i64);
-        assert_eq!(C, C - Per4th as i64 - Per5th as i64);
-        assert_eq!(C - Min2nd as i64, C + Maj7th as i64);
+        assert_eq!(C, G - Per5th);
+        assert_eq!(C, C - Octave);
+        assert_eq!(C, C - Per4th - Per5th);
+        assert_eq!(C - Min2nd, C + Maj7th);
     }
 }
