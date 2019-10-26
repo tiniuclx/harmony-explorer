9af1beb3795c3a97a82fb8e3f064c3207536bdf4
Alexandru Tiniuc <tiniuc.alexandru@gmail.com>
2019-10-26 11:20:21 +0100

    adds textual REPL

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
 extern crate enum_primitive_derive;
 extern crate num_traits;
+extern crate rustyline;
 
 mod music_theory;
 use pitch_calc::Letter::*;
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 
 fn main() {
-    println!("{:?}", C);
-    println!("{:?}", C + 7);
-    println!("{:?}", C + 12);
-    println!("{:?}", C + 48);
-    println!("{:?}", C - 2);
-    println!("{:?}", C - 7);
-    println!("{:?}", C - 22);
+    let mut rl = Editor::<()>::new();
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                println!("{}", line);
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
 }
