dc5ae6f5f4a3caf13abd9aa29124180335f38d7b
Alexandru Tiniuc <tiniuc.alexandru@gmail.com>
2019-10-26 19:46:17 +0100

    implements Chord type

--- src/music_theory.rs
+++ src/music_theory.rs
@@ -1,6 +1,13 @@
+pub use pitch_calc::Letter;
+pub use pitch_calc::{LetterOctave, Step};
+
+use pitch_calc::letter_octave_from_step;
+
+// TODO: implement Display for interval
+pub type Interval = i8;
 #[allow(dead_code, non_upper_case_globals)]
 pub mod interval {
-    type Interval = u8;
+    use super::*;
     pub const Root: Interval = 0;
     pub const Min2nd: Interval = 1;
     pub const Maj2nd: Interval = 2;
@@ -15,10 +22,96 @@ pub mod interval {
     pub const Maj7th: Interval = 11;
     pub const Octave: Interval = 12;
 }
+
+pub fn transpose(note: LetterOctave, interval: Interval) -> LetterOctave {
+    let interval_s = interval as pitch_calc::calc::Step;
+    let (letter, octave) = letter_octave_from_step(note.step() + interval_s);
+    LetterOctave(letter, octave)
+}
+
+// TODO: implement Display for Degree
+pub type Degree = u8;
+#[allow(dead_code)]
+pub mod degree {
+    use super::*;
+    pub const I: Degree = 1;
+    pub const II: Degree = 2;
+    pub const III: Degree = 3;
+    pub const IV: Degree = 4;
+    pub const V: Degree = 5;
+    pub const VI: Degree = 6;
+    pub const VII: Degree = 7;
+}
+
+pub type Quality = Vec<(Interval, Degree)>;
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct Chord {
+    root: LetterOctave,
+    quality: Quality,
+}
+
+impl Chord {
+    pub fn transposed(&self, interval: Interval) -> Chord {
+        let transposed = self.root.step() + interval as f32;
+        let (letter, octave) = letter_octave_from_step(transposed);
+        Chord {
+            root: LetterOctave(letter, octave),
+            quality: self.quality.clone(),
+        }
+    }
+
+    pub fn root(&self) -> LetterOctave {
+        self.root
+    }
+
+    pub fn with_root(&self, new_root: LetterOctave) -> Chord {
+        Chord {
+            root: new_root,
+            quality: self.quality.clone(),
+        }
+    }
+
+    pub fn root_letter(&self) -> Letter {
+        self.root.letter()
+    }
+
+    pub fn with_root_letter(&self, new_root: Letter) -> Chord {
+        Chord {
+            root: LetterOctave(new_root, self.root.octave()),
+            quality: self.quality.clone(),
+        }
+    }
+
+    pub fn quality(&self) -> Quality {
+        self.quality.clone()
+    }
+
+    pub fn with_quality(&self, quality: Quality) -> Chord {
+        Chord {
+            root: self.root,
+            quality: quality,
+        }
+    }
+
+    pub fn notes(&self) -> Vec<LetterOctave> {
+        self.quality
+            .clone()
+            .iter()
+            .map(|(i, _)| transpose(self.root, *i))
+            .fold(vec![self.root()], |mut ns, n| {
+                ns.push(n);
+                ns
+            })
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use degree::*;
     use interval::*;
+
     #[allow(unused_imports)]
     use pitch_calc::Letter::*;
 
@@ -32,5 +125,39 @@ mod tests {
         assert_eq!(C, C - Octave);
         assert_eq!(C, C - Per4th - Per5th);
         assert_eq!(C - Min2nd, C + Maj7th);
+    }
+
+    #[test]
+    #[allow(non_snake_case)]
+    fn chord_maths() {
+        let Cmaj = Chord {
+            root: LetterOctave(Letter::C, 4),
+            quality: vec![(Maj3rd, III), (Per5th, V)],
+        };
+
+        let Gmaj = Chord {
+            root: LetterOctave(Letter::G, 4),
+            quality: vec![(Maj3rd, III), (Per5th, V)],
+        };
+
+        let Amin = Chord {
+            root: LetterOctave(Letter::A, 4),
+            quality: vec![(Min3rd, III), (Per5th, V)],
+        };
+
+        assert_eq!(Gmaj, Cmaj.transposed(Per5th));
+        assert_eq!(Gmaj, Cmaj.with_root(LetterOctave(Letter::G, 4)));
+        assert_eq!(Gmaj, Cmaj.with_root_letter(Letter::G));
+
+        let Amin_generated = Cmaj
+            .transposed(Maj6th)
+            .with_quality(vec![(Min3rd, III), (Per5th, V)]);
+        assert_eq!(Amin, Amin_generated);
+
+        let notes_of_c_major: Vec<LetterOctave> = [Letter::C, Letter::E, Letter::G]
+            .iter()
+            .map(|l| LetterOctave(*l, 4))
+            .collect();
+        assert_eq!(Cmaj.notes(), notes_of_c_major);
     }
 }
