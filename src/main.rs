31ef250446f9f58f9d4dd47c47bef80baeb655b1
Alexandru Tiniuc <tiniuc.alexandru@gmail.com>
2019-10-26 12:34:26 +0100

    adds sampler example

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,77 @@ extern crate num_traits;
 extern crate rustyline;
 
 mod music_theory;
-use pitch_calc::Letter::*;
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
 
+extern crate find_folder; // For easily finding the assets folder.
+extern crate pitch_calc as pitch; // To work with musical notes.
+extern crate portaudio as pa; // For audio I/O
+extern crate sample; // To convert portaudio sample buffers to frames.
+extern crate sampler;
+
+use sampler::Sampler;
+
+const CHANNELS: i32 = 2;
+const SAMPLE_RATE: f64 = 44_100.0;
+const FRAMES_PER_BUFFER: u32 = 64;
+//const THUMB_PIANO: &'static str = "thumbpiano A#3.wav";
+const CASIO_PIANO: &'static str = "Casio Piano C5.wav";
+
+fn run() -> Result<(), pa::Error> {
+    // We'll create a sample map that maps a single sample to the entire note range.
+    let assets = find_folder::Search::ParentsThenKids(5, 5)
+        .for_folder("assets")
+        .unwrap();
+    let sample = sampler::Sample::from_wav_file(assets.join(CASIO_PIANO), SAMPLE_RATE).unwrap();
+    let sample_map = sampler::Map::from_single_sample(sample);
+
+    // Create a polyphonic sampler.
+    let mut sampler = Sampler::poly((), sample_map).num_voices(4);
+
+    // Initialise PortAudio and create an output stream.
+    let pa = pa::PortAudio::new()?;
+    println!("Device count: {:?}", pa.device_count());
+    let settings =
+        pa.default_output_stream_settings::<f32>(CHANNELS, SAMPLE_RATE, FRAMES_PER_BUFFER)?;
+
+    let callback = move |pa::OutputStreamCallbackArgs { buffer, .. }| {
+        let buffer: &mut [[f32; CHANNELS as usize]] =
+            sample::slice::to_frame_slice_mut(buffer).unwrap();
+        sample::slice::equilibrium(buffer);
+
+        // If the sampler is not currently active, play a note.
+        if !sampler.is_active() {
+            let vel = 0.3;
+            sampler.note_on(pitch::LetterOctave(pitch::Letter::C, 4).to_hz(), vel);
+            sampler.note_on(pitch::LetterOctave(pitch::Letter::E, 4).to_hz(), vel);
+            sampler.note_on(pitch::LetterOctave(pitch::Letter::G, 4).to_hz(), vel);
+        }
+
+        sampler.fill_slice(buffer, SAMPLE_RATE);
+
+        pa::Continue
+    };
+
+    let mut stream = pa.open_non_blocking_stream(settings, callback)?;
+
+    stream.start()?;
+
+    while let Ok(true) = stream.is_active() {
+        std::thread::sleep(std::time::Duration::from_millis(16));
+    }
+
+    stream.stop()?;
+    stream.close()?;
+
+    Ok(())
+}
+
 fn main() {
     let mut rl = Editor::<()>::new();
+
+    run().unwrap();
+
     loop {
         let readline = rl.readline("♪♪♪ ");
         match readline {
