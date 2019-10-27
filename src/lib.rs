eb99eec351f7b74d6d01aeb3174f4c696a4b6a3c
Alexandru Tiniuc <tiniuc.alexandru@gmail.com>
2019-10-27 10:13:51 +0000

    export library

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate diesel_migrations;
+extern crate enum_primitive_derive;
+extern crate find_folder; // For easily finding the assets folder.
+extern crate gag;
+extern crate num_traits;
+extern crate pitch_calc; // To work with musical notes.
+extern crate portaudio; // For audio I/O
+extern crate rustyline;
+extern crate sample; // To convert portaudio sample buffers to frames.
+extern crate sampler;
+
+pub mod database;
+pub mod music_theory;
+pub mod schema;
